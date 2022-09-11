8cbc1ce8d6317a522fd6c86461bedf22b22bd58c
Maikeru <78266557+MaikeruDev@users.noreply.github.com>
2022-09-11 19:18:09 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Impossible Form',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
